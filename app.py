b0a5dc1f7c247dec5fe2c64f2d87364d97aafd19
thomasmcquillan <thomas.mcquillan@gmail.com>
2022-02-07 16:48:01 +0000

    Edit app.py adding mongo database configuration settings and connection variables

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-# Adds variable 'app', to store new instance of Flask. 
+# Adds variable 'app', to store new instance of Flask
 
 app = Flask(__name__)
+
+# Mongo database configuration settings
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+# Setting variable to retrieve secret access key
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# Mongo database connection
+mongo = PyMongo(app)
+
