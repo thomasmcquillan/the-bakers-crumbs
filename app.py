7e33b5f0da7d2e756a1925ea9d327f4c5b6b02bc
thomasmcquillan <thomas.mcquillan@gmail.com>
2022-02-15 18:00:55 +0000

    Add index.html, edit templates and add recipe search function to app.py

--- app.py
+++ app.py
@@ -4,42 +4,43 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-# Adds variable 'app', to store new instance of Flask
 
+# Setting Config variables
 app = Flask(__name__)
 
-# Mongo database configuration settings
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-# Setting variable to retrieve secret access key
 app.secret_key = os.environ.get("SECRET_KEY")
 
-# Mongo database connection
 mongo = PyMongo(app)
 
 
-# Home Page
 @app.route("/")
+def index():
+    """ Returns user to Homepage """
+    return render_template("index.html")
+
+
 @app.route("/get_recipes")
 def get_recipes():
-
-    total = mongo.db.recipes.find().count()
+    """ Displays recipes from MongoDB Database """
     recipes = mongo.db.recipes.find()
-    return render_template("index.html", recipes=recipes)
+    return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    """ Search recipes function """
+    query = request.form.get("query")
+    recipes = mongo.db.recipes.find({"$text": {"$search": query}})
+    return render_template("searchresults.html", recipes=recipes)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-# Search recipes function
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    query = request.form.get("query")
-    recipes = mongo.db.recipes.find({"$text": {"$search": query}})
-    return render_template("recipes.html", recipes=recipes)
-
