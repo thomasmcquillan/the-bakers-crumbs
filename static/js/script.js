32aceca0442f163e3d05df8a425cc948540d7bba
thomasmcquillan <thomas.mcquillan@gmail.com>
2022-03-23 16:20:37 +0000

    Edit layout styles

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+const searchbutton = document.getElementById("search-btn");
+const butt = document.getElementById("query");
+
+$(document).ready(function() {
+	$("butt").click(function() {
+		$(".main").toggle();
+    });
+});
